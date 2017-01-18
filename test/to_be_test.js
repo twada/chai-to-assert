54a1b5a423d2ecb0fe474a7f4a76a1b60973311c
Takuto Wada <takuto.wada@gmail.com>
2017-01-18 17:30:15 +0900

    feat(chai-to-assert): chai-to-assert codemod begins

---
+++ test/to_be_test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var assert = require('assert');
+var jscodeshift = require('jscodeshift');
+var tranform = require('../lib/expect-to-assert');
+var noop = function () {};
+
+it('expect(foo).to.be.ok', function () {
+    var input = 'expect(foo).to.be.ok';
+    var output = tranform({path: 'test.js', source: input}, {jscodeshift: jscodeshift, stats: noop});
+    assert.equal(output, 'assert(foo)');
+});
