3a504bbaba5538432c423f4961a66fe836218d44
Takuto Wada <takuto.wada@gmail.com>
2017-01-23 11:13:44 +0900

    refactor(chai-to-assert): organize subtests

--- test/to_be_test.js
+++ test/to_be_test.js
@@ -12,210 +12,193 @@ function testTransform (opts) {
     });
 }
 
-testTransform({
-    before: 'expect(foo).to.be.ok',
-    after:  'assert(foo)'
+describe('.ok', function () {
+    testTransform({
+        before: 'expect(foo).to.be.ok',
+        after:  'assert(foo)'
+    });
+    testTransform({
+        before: 'expect(foo).to.not.be.ok',
+        after:  'assert(!foo)'
+    });
+    testTransform({
+        before: 'expect(foo).not.to.be.ok',
+        after:  'assert(!foo)'
+    });
+    testTransform({
+        before: 'expect(foo).have.to.be.ok',
+        after:  'assert(foo)'
+    });
 });
 
-testTransform({
-    before: 'expect(foo).to.not.be.ok',
-    after:  'assert(!foo)'
+describe('.true', function () {
+    testTransform({
+        before: 'expect(foo).to.be.true',
+        after:  'assert(foo === true)'
+    });
+    testTransform({
+        before: 'expect(foo).to.not.be.true',
+        after:  'assert(foo !== true)'
+    });
+    testTransform({
+        before: 'expect(true).to.be.true',
+        after:  'assert(true === true)'
+    });
+    testTransform({
+        before: 'expect(1).to.not.be.true',
+        after:  'assert(1 !== true)'
+    });
 });
 
-testTransform({
-    before: 'expect(foo).not.to.be.ok',
-    after:  'assert(!foo)'
+describe('.false', function () {
+    testTransform({
+        before: 'expect(foo).to.be.false',
+        after:  'assert(foo === false)'
+    });
+    testTransform({
+        before: 'expect(foo).to.not.be.false',
+        after:  'assert(foo !== false)'
+    });
+    testTransform({
+        before: 'expect(false).to.be.false',
+        after:  'assert(false === false)'
+    });
+    testTransform({
+        before: 'expect(0).to.not.be.false',
+        after:  'assert(0 !== false)'
+    });
 });
 
-testTransform({
-    before: 'expect(foo).have.to.be.ok',
-    after:  'assert(foo)'
+describe('.null', function () {
+    testTransform({
+        before: 'expect(null).to.be.null',
+        after:  'assert(null === null)'
+    });
+    testTransform({
+        before: 'expect(undefined).to.not.be.null',
+        after:  'assert(undefined !== null)'
+    });
 });
 
-testTransform({
-    before: 'expect(foo).to.be.true',
-    after:  'assert(foo === true)'
+describe('.undefined', function () {
+    testTransform({
+        before: 'expect(undefined).to.be.undefined',
+        after:  'assert(undefined === undefined)'
+    });
+    testTransform({
+        before: 'expect(null).to.not.be.undefined',
+        after:  'assert(null !== undefined)'
+    });
 });
 
-testTransform({
-    before: 'expect(foo).to.not.be.true',
-    after:  'assert(foo !== true)'
+describe('.equal()', function () {
+    testTransform({
+        before: 'expect("hello").to.equal("hello")',
+        after:  'assert("hello" === "hello")'
+    });
+    testTransform({
+        before: 'expect(42).to.equal(42)',
+        after:  'assert(42 === 42)'
+    });
+    testTransform({
+        before: 'expect(1).to.not.equal(true)',
+        after:  'assert(1 !== true)'
+    });
+    testTransform({
+        before: 'expect({ foo: "bar" }).to.not.equal({ foo: "bar" })',
+        after:  'assert({ foo: "bar" } !== { foo: "bar" })'
+    });
+    testTransform({
+        before: 'expect({ foo: "bar" }).to.deep.equal({ foo: "bar" })',
+        after:  'assert.deepStrictEqual({ foo: "bar" }, { foo: "bar" })'
+    });
+    testTransform({
+        before: 'expect(foo).to.not.deep.equal(baz)',
+        after:  'assert.notDeepStrictEqual(foo, baz)'
+    });
+    testTransform({
+        before: 'expect(foo).to.equals(bar)',
+        after:  'assert(foo === bar)'
+    });
+    testTransform({
+        before: 'expect(foo).to.not.equals(bar)',
+        after:  'assert(foo !== bar)'
+    });
+    testTransform({
+        before: 'expect(foo).to.eq(bar)',
+        after:  'assert(foo === bar)'
+    });
+    testTransform({
+        before: 'expect(foo).to.not.eq(bar)',
+        after:  'assert(foo !== bar)'
+    });
 });
 
-testTransform({
-    before: 'expect(foo).to.be.false',
-    after:  'assert(foo === false)'
+describe('.eql()', function () {
+    testTransform({
+        before: 'expect({ foo: "bar" }).to.eql({ foo: "bar" })',
+        after:  'assert.deepStrictEqual({ foo: "bar" }, { foo: "bar" })'
+    });
+    testTransform({
+        before: 'expect({ foo: "bar" }).to.not.eql({ foo: "baz" })',
+        after:  'assert.notDeepStrictEqual({ foo: "bar" }, { foo: "baz" })'
+    });
+    testTransform({
+        before: 'expect(foo).to.eqls(bar)',
+        after:  'assert.deepStrictEqual(foo, bar)'
+    });
+    testTransform({
+        before: 'expect(foo).to.not.eqls(bar)',
+        after:  'assert.notDeepStrictEqual(foo, bar)'
+    });
 });
 
-testTransform({
-    before: 'expect(foo).to.not.be.false',
-    after:  'assert(foo !== false)'
+describe('.a(), .an()', function () {
+    testTransform({
+        before: 'expect("test").to.be.a("string")',
+        after:  'assert(typeof "test" === "string")'
+    });
+    testTransform({
+        before: 'expect(foo).to.not.be.a("string")',
+        after:  'assert(typeof foo !== "string")'
+    });
+    testTransform({
+        before: 'expect({ foo: "bar" }).to.be.an("object")',
+        after:  'assert(typeof { foo: "bar" } === "object")'
+    });
+    testTransform({
+        before: 'expect(undefined).to.be.an("undefined")',
+        after:  'assert(typeof undefined === "undefined")'
+    });
+    testTransform({
+        before: 'expect(Symbol()).to.be.a("symbol")',
+        after:  'assert(typeof Symbol() === "symbol")'
+    });
+    testTransform({
+        before: 'expect(foo).to.be.a("null")',
+        after:  'assert(foo === null)'
+    });
+    testTransform({
+        before: 'expect(foo).to.not.be.a("null")',
+        after:  'assert(foo !== null)'
+    });
 });
 
-
-// from Chai API Reference
-
-testTransform({
-    before: 'expect(true).to.be.true',
-    after:  'assert(true === true)'
-});
-
-testTransform({
-    before: 'expect(1).to.not.be.true',
-    after:  'assert(1 !== true)'
-});
-
-testTransform({
-    before: 'expect(false).to.be.false',
-    after:  'assert(false === false)'
-});
-
-testTransform({
-    before: 'expect(0).to.not.be.false',
-    after:  'assert(0 !== false)'
-});
-
-testTransform({
-    before: 'expect(null).to.be.null',
-    after:  'assert(null === null)'
-});
-
-testTransform({
-    before: 'expect(undefined).to.not.be.null',
-    after:  'assert(undefined !== null)'
-});
-
-testTransform({
-    before: 'expect(undefined).to.be.undefined',
-    after:  'assert(undefined === undefined)'
-});
-
-testTransform({
-    before: 'expect(null).to.not.be.undefined',
-    after:  'assert(null !== undefined)'
-});
-
-testTransform({
-    before: 'expect("hello").to.equal("hello")',
-    after:  'assert("hello" === "hello")'
-});
-
-testTransform({
-    before: 'expect(foo).to.equals(bar)',
-    after:  'assert(foo === bar)'
-});
-
-testTransform({
-    before: 'expect(foo).to.not.equals(bar)',
-    after:  'assert(foo !== bar)'
-});
-
-testTransform({
-    before: 'expect(foo).to.eq(bar)',
-    after:  'assert(foo === bar)'
-});
-
-testTransform({
-    before: 'expect(foo).to.not.eq(bar)',
-    after:  'assert(foo !== bar)'
-});
-
-testTransform({
-    before: 'expect(42).to.equal(42)',
-    after:  'assert(42 === 42)'
-});
-
-testTransform({
-    before: 'expect(1).to.not.equal(true)',
-    after:  'assert(1 !== true)'
-});
-
-testTransform({
-    before: 'expect({ foo: "bar" }).to.not.equal({ foo: "bar" })',
-    after:  'assert({ foo: "bar" } !== { foo: "bar" })'
-});
-
-testTransform({
-    before: 'expect({ foo: "bar" }).to.deep.equal({ foo: "bar" })',
-    after:  'assert.deepStrictEqual({ foo: "bar" }, { foo: "bar" })'
-});
-
-testTransform({
-    before: 'expect(foo).to.not.deep.equal(baz)',
-    after:  'assert.notDeepStrictEqual(foo, baz)'
-});
-
-testTransform({
-    before: 'expect({ foo: "bar" }).to.eql({ foo: "bar" })',
-    after:  'assert.deepStrictEqual({ foo: "bar" }, { foo: "bar" })'
-});
-
-testTransform({
-    before: 'expect({ foo: "bar" }).to.not.eql({ foo: "baz" })',
-    after:  'assert.notDeepStrictEqual({ foo: "bar" }, { foo: "baz" })'
-});
-
-testTransform({
-    before: 'expect(foo).to.eqls(bar)',
-    after:  'assert.deepStrictEqual(foo, bar)'
-});
-
-testTransform({
-    before: 'expect(foo).to.not.eqls(bar)',
-    after:  'assert.notDeepStrictEqual(foo, bar)'
-});
-
-testTransform({
-    before: 'expect("test").to.be.a("string")',
-    after:  'assert(typeof "test" === "string")'
-});
-
-testTransform({
-    before: 'expect(foo).to.not.be.a("string")',
-    after:  'assert(typeof foo !== "string")'
-});
-
-testTransform({
-    before: 'expect({ foo: "bar" }).to.be.an("object")',
-    after:  'assert(typeof { foo: "bar" } === "object")'
-});
-
-testTransform({
-    before: 'expect(undefined).to.be.an("undefined")',
-    after:  'assert(typeof undefined === "undefined")'
-});
-
-testTransform({
-    before: 'expect(Symbol()).to.be.a("symbol")',
-    after:  'assert(typeof Symbol() === "symbol")'
-});
-
-testTransform({
-    before: 'expect(foo).to.be.a("null")',
-    after:  'assert(foo === null)'
-});
-
-testTransform({
-    before: 'expect(foo).to.not.be.a("null")',
-    after:  'assert(foo !== null)'
-});
-
-testTransform({
-    before: 'expect([ 1, 2, 3 ]).to.be.instanceof(Array)',
-    after:  'assert([ 1, 2, 3 ] instanceof Array)'
-});
-
-testTransform({
-    before: 'expect(foo).to.not.be.instanceof(Array)',
-    after:  'assert(!(foo instanceof Array))'
-});
-
-testTransform({
-    before: 'expect(foo).to.be.instanceOf(Array)',
-    after:  'assert(foo instanceof Array)'
-});
-
-testTransform({
-    before: 'expect(bar).to.not.be.instanceOf(Array)',
-    after:  'assert(!(bar instanceof Array))'
+describe('.instanceof()', function () {
+    testTransform({
+        before: 'expect([ 1, 2, 3 ]).to.be.instanceof(Array)',
+        after:  'assert([ 1, 2, 3 ] instanceof Array)'
+    });
+    testTransform({
+        before: 'expect(foo).to.not.be.instanceof(Array)',
+        after:  'assert(!(foo instanceof Array))'
+    });
+    testTransform({
+        before: 'expect(foo).to.be.instanceOf(Array)',
+        after:  'assert(foo instanceof Array)'
+    });
+    testTransform({
+        before: 'expect(bar).to.not.be.instanceOf(Array)',
+        after:  'assert(!(bar instanceof Array))'
+    });
 });
